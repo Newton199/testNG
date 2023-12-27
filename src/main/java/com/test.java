dd84a2dc821ea649233ac484deaccb621bbc06c7
Newton199 <NewtonRajKaphle@kbc.edu.np>
2023-12-27 10:56:36 +0545

    n

--- src/main/java/com/test.java
+++ src/main/java/com/test.java
@@ -6,29 +6,24 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
 
-
-
 public class test {
-     public static void main(String[] args) {
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
-        options.addArguments("--remote-allow-origins=*");
-        ChromeDriver driver = new ChromeDriver(options);
-        driver.manage().window().maximize();
-        driver.manage().deleteAllCookies();
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+  public static void main(String[] args) {
+    ChromeOptions options = new ChromeOptions();
+    options.setBinary("C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe");
+    options.addArguments("--remote-allow-origins=*");
+    ChromeDriver driver = new ChromeDriver(options);
+    driver.manage().window().maximize();
+    driver.manage().deleteAllCookies();
+    driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
 
+    String url = "https://chroma-tech-academy.mexil.it/static_page/";
+    driver.get(url);
 
-        String url = "https://chroma.mexil.it/site/login";
-        driver.get(url);
+    String actualPageTitle = driver.getTitle();
+    String expectedPageTitle = "Chroma Tech Academy";
+    Assert.assertEquals(actualPageTitle, expectedPageTitle, "---VERIFYING PAGE TITLE---");
 
-        String actualPageTitle = driver.getTitle();
-        String expectedPageTitle ="Chroma Tech Academy";
-        Assert.assertEquals(actualPageTitle, expectedPageTitle);
-        
-     // Assert.assertEquals(actualPageTitle, expectedPageTitle);
-        
+    driver.quit();
 
-        
-}
+  }
 }
